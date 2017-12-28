e5f4c7433762f1fd69d062c6bde6483d5c2e29f9
LORDBABUINO <jorge.x7@gmail.com>
2017-12-28 18:49:53 -0200

    Creating access components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AccessComponent } from './access/access.component';
+import { BannerComponent } from './access/banner/banner.component';
+import { LoginComponent } from './access/login/login.component';
+import { SignUpComponent } from './access/sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccessComponent,
+    BannerComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
