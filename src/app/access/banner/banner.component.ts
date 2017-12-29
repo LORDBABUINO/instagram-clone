ebe5fdeff71be9c951de3d8bd9f670ffd0f75257
LORDBABUINO <jorge.x7@gmail.com>
2017-12-28 22:55:36 -0200

    Initializing banner animation

--- src/app/access/banner/banner.component.ts
+++ src/app/access/banner/banner.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('hidden', style({
+        opacity: 0
+      })),
+      state('visible', style({
+        opacity: 1
+      }))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public state: string = 'hidden'
 
   constructor() { }
 
