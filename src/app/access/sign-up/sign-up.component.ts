bbd22e88190dcab10ec313a30e2ae0e71bf468b0
LORDBABUINO <jorge.x7@gmail.com>
2017-12-30 02:01:11 -0200

    Including login link

--- src/app/access/sign-up/sign-up.component.ts
+++ src/app/access/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  @Output() public showPanel: EventEmitter<boolean> = new EventEmitter<boolean>()
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public showLogin(): void{
+    this.showPanel.emit(true)
+  }
 }
