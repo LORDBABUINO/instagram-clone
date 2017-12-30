3b091741d87182c8f3fed8cb60669ab74bf90977
LORDBABUINO <jorge.x7@gmail.com>
2017-12-30 00:34:03 -0200

    Animating panel creation

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -14,12 +14,22 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         style({opacity: 0, transform: 'translate(-50px, 0px)'}),
         animate('500ms 0s ease-in-out')
       ])
+    ]),
+    trigger('animation-panel', [
+      state('created', style({
+        opacity: 1
+      })),
+      transition('void => created', [
+        style({opacity: 0, transform: 'translate(50px, 0px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
     ])
   ]
 })
 export class AccessComponent implements OnInit {
 
   public stateBanner: string = 'created'
+  public statePanel: string = 'created'
 
   constructor() { }
 
