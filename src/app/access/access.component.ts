7e75fbd200e66c0ec193e35b29f09f23d2de1328
LORDBABUINO <jorge.x7@gmail.com>
2017-12-30 00:24:08 -0200

    Animating banner creation

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-access',
   templateUrl: './access.component.html',
-  styleUrls: ['./access.component.css']
+  styleUrls: ['./access.component.css'],
+  animations: [
+    trigger('animation-banner', [
+      state('created', style({
+        opacity: 1
+      })),
+      transition('void => created', [
+        style({opacity: 0, transform: 'translate(-50px, 0px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AccessComponent implements OnInit {
+
+  public stateBanner: string = 'created'
 
   constructor() { }
 
