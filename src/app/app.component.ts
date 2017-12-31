83e5ae46ae3eecb8a5803a0ff988b8a619223e35
LORDBABUINO <jorge.x7@gmail.com>
2017-12-30 22:54:04 -0200

    Configuring firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void{
+    let config = {
+      apiKey: "AIzaSyAZwdYG1RGo94_4swqoClbwtbq4oWsgsOA",
+      authDomain: "angular-instagram-clone.firebaseapp.com",
+      databaseURL: "https://angular-instagram-clone.firebaseio.com",
+      projectId: "angular-instagram-clone",
+      storageBucket: "angular-instagram-clone.appspot.com",
+      messagingSenderId: "632889734859"
+    };
+    firebase.initializeApp(config);
+  }
 }
