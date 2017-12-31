fcd9a4e1339a86612e3d3cf36f0cb97a2ffbe016
LORDBABUINO <jorge.x7@gmail.com>
2017-12-30 23:13:49 -0200

    Add sign up data to database

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,16 @@
 import { User } from './access/user.model'
+import * as firebase from 'firebase'
 
 export class Auth {
   public signUp(user: User): void {
     console.log(user)
+
+    firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+      .then((response: any) => {
+        console.log(response)
+      })
+      .catch((error: Error) => {
+        console.log(error)
+      })
   }
 }
