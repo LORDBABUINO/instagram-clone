c8612c9e8f16bb8967b8dc668fb4db46bc38703f
LORDBABUINO <jorge.x7@gmail.com>
2018-01-07 20:24:28 -0200

    Checking authentication

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,7 +1,13 @@
+import { Injectable } from '@angular/core'
 import { CanActivate } from '@angular/router'
+import { Auth } from './auth.service'
 
+@Injectable()
 export class AuthGuard {
+
+  constructor(private auth: Auth){}
+
   canActivate(): boolean {
-    return true
+    return this.auth.tokenId !== undefined
   }
 }
